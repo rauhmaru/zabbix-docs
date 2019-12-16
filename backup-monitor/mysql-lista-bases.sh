06ea8fb8449e45e434c6f74486d4ed6256e68e99
Raul Libório <rauhmaru@gmail.com>
2019-12-16 11:41:06 -0300

    Update mysql-lista-bases.sh

--- backup-monitor/mysql-lista-bases.sh
+++ backup-monitor/mysql-lista-bases.sh
@@ -2,23 +2,20 @@
 
 # Script para listagem e montagem do JSON com as bases de dados
 # Raul Libório, rauhmaru@opensuse.org / gmail.com
-# 
 
-# Credenciais
+# Credenciais do banco
 User=operador
 Pwd="operador"
 
-# Arquivo de saida
-json_file="file.json"
-
 # Adicione aqui as bases de dados que nao queira que sejam monitoradas, separadas por pipe "|"
 ## 1. Não remova a primeira palvra "Database"
-## 2. 
 
 BasesExcluidas="Database|information_schema|lost\+found|performance_schema"
 
+# Listagem das bases
 ArrayBancos=($(mysql -u $User -hlocalhost -p$Pwd -e 'show databases;'|egrep -v "$BasesExcluidas"))
 
+# Construção do JSON
 echo -e "{\n\t\"data\":[\n"
 Final=$(( ${#ArrayBancos[@]} - 1 ))
 for Contador in $( seq 0 $Final ); do
