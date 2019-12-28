e0fcbf54e8d43d350fdf4e66a3cce2d7ba1977a4
Raul Libório <rauhmaru@gmail.com>
2019-12-27 22:50:07 -0300

    Create systemd_discovery_services.sh

---
+++ systemd_services_monitor/systemd_discovery_services.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Lista os servicos e verifica se estao em execucao, usando systemd
+
+tmpfile=$( mktemp )
+jsonFile=$( mktemp )
+
+# Todos os servicos
+systemctl list-units --no-page -t service -a | awk /.service/'{ gsub(".service","");gsub("^..",""); print }' > $tmpfile
+
+# Criacao do json
+TotalDeLinhas=$( wc -l < $tmpfile )
+Final=$(( TotalDeLinhas -1  ))
+	
+# Criacao do arquivo json
+echo -e "{\n  \"data\":["
+
+# Loop para composicao do arquivo json
+while read Unit Load Active Sub Description; do
+	echo -e "    {
+      \"{#SERVICE_NAME}\":\"$Unit\",
+      \"{#SERVICE_LOAD}\":\"$Load\",
+      \"{#SERVICE_ACTIVE}\":\"$Active\",
+      \"{#SERVICE_SUB}\":\"$Sub\",
+      \"{#SERVICE_DESCRIPTION}\":\"$Description\""
+	
+	let Contador++
+		if [[ $Contador -le $Final ]]; then
+			echo -e "    },"
+	else
+			echo -e "    }\n  ]\n}"
+	fi
+done < $tmpfile
+	
+rm -f $tmpfile $jsonfile
