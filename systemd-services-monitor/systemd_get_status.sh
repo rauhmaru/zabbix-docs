296e483441a9e5c96353ebeccf2b04d9e2e395ad
Raul Libório <rauhmaru@gmail.com>
2019-12-28 01:10:34 -0300

    Update systemd_get_status.sh

--- systemd-services-monitor/systemd_get_status.sh
+++ systemd-services-monitor/systemd_get_status.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Verifica o status dos servicos, baseados em systemd
+# Raul Liborio, rauhmaru@opensuse.org / gmail.com
+# v0.1 - 27/01/2019
+
 
 Service=$1
 systemctl list-units --no-page -t service -a | awk /.service/'{{ gsub(".service","");gsub("^..",""); if ( $1 == "'$Service'" ) print $4 }}'
