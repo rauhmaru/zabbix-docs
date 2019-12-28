25fa4fcc4dac96718e632369db521685f4822163
Raul Libório <rauhmaru@gmail.com>
2019-12-28 01:10:16 -0300

    Update systemd_get_activity.sh

--- systemd-services-monitor/systemd_get_activity.sh
+++ systemd-services-monitor/systemd_get_activity.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Verifica se os servicos do systemd estao ativos
+# Raul Liborio, rauhmaru@opensuse.org / gmail.com
+# v0.1 - 27/01/2019
 
 Service=$1
 systemctl list-units --no-page -t service -a | awk /.service/'{{ gsub(".service","");gsub("^..",""); if ( $1 == "'$Service'" ) print $3 }}'
