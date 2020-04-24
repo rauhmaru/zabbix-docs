e4d22c4500830359154102c796badf70b941e0c1
Raul Libório <rauhmaru@gmail.com>
2020-04-24 03:32:38 -0300

    Create dp_discovery_dcbf.sh

---
+++ data-protector/dp_discovery_dcbf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# LLD to find DCBF directories size
+
+OLDLANG="$LANG"
+LANG="en_US.utf8"
+tmpfile=$( mktemp )
+ReportFile="/tmp/omnirpt_rpt_dbsize.out"
+
+awk '/Detail Catalog Binary Files/,0{ if ($1 ~ /dcbf/ && $3 ~ /[0-9]$/ ) print $1,$NF}' $ReportFile > $tmpfile
+
+
+# Criacao do json
+TotalDeLinhas=$( wc -l < $tmpfile )
+Final=$(( TotalDeLinhas -1  ))
+
+sed -i 's/ .*//g;' $tmpfile
+# Loop para composicao do arquivo json
+echo -e "{\n\t\"data\":[\n"
+
+while read DCBF; do
+echo -e "  {"
+                echo -e "    \"{#DCBF}\":\"$DCBF\""
+                let Contador++
+                        if [[ $Contador -le $Final ]]; then
+                                echo -e "  },\n"
+                else
+                                echo -e "  }\n]\n}"
+                fi
+done < $tmpfile
+
+
+
+rm -f $tmpfile
+
+LANG=$OLDLANG
+
+exit 0
